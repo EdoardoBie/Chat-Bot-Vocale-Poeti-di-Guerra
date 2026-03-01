e3b5d6eeb22e7ceebe59106ffb1a67e479bae6e6
Edoardo Biestro <167993193+EdoardoBie@users.noreply.github.com>
2026-03-01 22:01:59 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useEffect } from 'react';
 import ChatInterface from './components/ChatInterface';
 
 export default function App() {
+  useEffect(() => {
+    console.log("App mounted");
+  }, []);
+
   return <ChatInterface />;
 }
